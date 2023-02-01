a72e30e53bf74b7066cbe41375a711814519a2d7
Victor Jesus Romero Perez <vcoromero@gmail.com>
2023-02-01 14:31:42 -0600

    creating user model and passing data to view

--- src/main/java/com/bolsaideas/springboot/app/springbootweb/controllers/IndexController.java
+++ src/main/java/com/bolsaideas/springboot/app/springbootweb/controllers/IndexController.java
@@ -5,13 +5,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bolsaideas.springboot.app.springbootweb.models.User;
+
 @Controller
 @RequestMapping("/app")
 public class IndexController {
 
     @GetMapping({"","/","/index","/home"})
     public String index(Model model){
-        model.addAttribute("titulo", "Hola spring framework");
+        model.addAttribute("title", "Hola spring framework");
         return "index";
+    }
+
+    @RequestMapping("/profile")
+    public String profile(Model model){
+        User user = new User();
+        user.setName("John");
+        user.setSurname("Doe");
+        model.addAttribute("title", "El nombre del usuario es "+user.getName());
+        model.addAttribute("user", user);
+        model.addAttribute("surname", user);
+        return "profile";
     }
 }
