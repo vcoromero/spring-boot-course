3fb34445243509244f9136019f091f224867cc41
Victor Jesus Romero Perez <vcoromero@gmail.com>
2023-02-01 14:10:26 -0600

    how to map controller

--- src/main/java/com/bolsaideas/springboot/app/springbootweb/controllers/IndexController.java
+++ src/main/java/com/bolsaideas/springboot/app/springbootweb/controllers/IndexController.java
@@ -3,11 +3,13 @@ package com.bolsaideas.springboot.app.springbootweb.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
-    @GetMapping({"/","/index","/home"})
+    @GetMapping({"","/","/index","/home"})
     public String index(Model model){
         model.addAttribute("titulo", "Hola spring framework");
         return "index";
