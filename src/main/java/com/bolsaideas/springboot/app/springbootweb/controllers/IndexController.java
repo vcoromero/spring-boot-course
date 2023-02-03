9fcef251e181d95cedbd01c4ca4ab8ca671ab2cf
Victor Jesus Romero Perez <vcoromero@gmail.com>
2023-02-03 09:31:52 -0600

    lear how to user modelAttribute

--- src/main/java/com/bolsaideas/springboot/app/springbootweb/controllers/IndexController.java
+++ src/main/java/com/bolsaideas/springboot/app/springbootweb/controllers/IndexController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bolsaideas.springboot.app.springbootweb.models.User;
@@ -33,12 +34,20 @@ public class IndexController {
 
     @GetMapping("/user-list")
     public String list(Model model){
+        model.addAttribute("title", "User list");
+        return "user-list";
+    }
+    
+    /**
+     * con esta manera podemos retornar usuarios en diferentes vistas
+     * @return
+     */
+    @ModelAttribute("users")
+    public List<User> setUsers(){
         List<User> users = new ArrayList<>();
         users.add(new User("John", "Doe", "john@doe.com"));
         users.add(new User("Jane", "Doe", "jane@doe.com"));
         users.add(new User("Tornado", "Doe", "tornado@doe.com"));
-        model.addAttribute("title", "User list");
-        model.addAttribute("users", users);
-        return "user-list";
+        return users;
     }
 }
