3d1e5292253715e12f2e4113b6100634b5da5f10
Victor Jesus Romero Perez <vcoromero@gmail.com>
2023-03-02 12:47:29 -0600

    how to send request params using httpServletRequest

--- src/main/java/com/bolsaideas/springboot/app/springbootweb/controllers/ParamsExampleController.java
+++ src/main/java/com/bolsaideas/springboot/app/springbootweb/controllers/ParamsExampleController.java
@@ -6,19 +6,42 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/params")
 public class ParamsExampleController {
 
-    @GetMapping("/")
-    public String index(){
+    @GetMapping({ "", "/" })
+    public String index() {
         return "params/index";
     }
 
     @GetMapping("/string")
-    public String param(@RequestParam(name = "text", required = false, defaultValue = "default") String text, Model model){
-        model.addAttribute("result", "El texto enviado es: "+text);
+    public String param(@RequestParam(name = "text", required = false, defaultValue = "default") String text,
+            Model model) {
+        model.addAttribute("result", "El texto enviado es: " + text);
+        return "params/see";
+    }
+
+    @GetMapping("/mix-params")
+    public String param(@RequestParam() String greeting, @RequestParam() Integer number,
+            Model model) {
+        model.addAttribute("result", "El texto enviado es: " + greeting + "' y el numero es '" + number + "'");
         return "params/see";
     }
     
+    @GetMapping("/mix-params-request")
+    public String param(HttpServletRequest request, Model model) {
+        String greeting = request.getParameter("greeting");
+        Integer number = null;
+        try {
+            number = Integer.parseInt(request.getParameter("number"));
+        } catch (Exception e) {
+            number = 0;
+        }
+        model.addAttribute("result", "El texto enviado es: " + greeting + "' y el numero es '" + number + "'");
+        return "params/see";
+    }
+
 }
