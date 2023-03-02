5ed3fcce070ecea27893999c40b9bb4964d5dd4c
Victor Jesus Romero Perez <vcoromero@gmail.com>
2023-03-02 13:27:12 -0600

    dependency injections

--- src/main/java/com/bolsaideas/springboot/app/springbootweb/controllers/IndexController.java
+++ src/main/java/com/bolsaideas/springboot/app/springbootweb/controllers/IndexController.java
@@ -3,6 +3,7 @@ package com.bolsaideas.springboot.app.springbootweb.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,35 +16,45 @@ import com.bolsaideas.springboot.app.springbootweb.models.User;
 @RequestMapping("/app")
 public class IndexController {
 
-    @GetMapping({"","/","/index","/home"})
-    public String index(Model model){
-        model.addAttribute("title", "Hola spring framework");
+    @Value("${text.indexController.index.title}")
+    private String indexText;
+
+    @Value("${text.indexController.profile.title}")
+    private String profileText;
+
+    @Value("${text.indexController.list.title}")
+    private String listText;
+
+    @GetMapping({ "", "/", "/index", "/home" })
+    public String index(Model model) {
+        model.addAttribute("title", indexText);
         return "index";
     }
 
     @RequestMapping("/profile")
-    public String profile(Model model){
+    public String profile(Model model) {
         User user = new User();
         user.setName("John");
         user.setSurname("Doe");
         user.setEmail("john@doe.com");
-        model.addAttribute("title", "El nombre del usuario es "+user.getName());
+        model.addAttribute("title", profileText + user.getName());
         model.addAttribute("user", user);
         return "profile";
     }
 
     @GetMapping("/user-list")
-    public String list(Model model){
-        model.addAttribute("title", "User list");
+    public String list(Model model) {
+        model.addAttribute("title", listText);
         return "user-list";
     }
-    
+
     /**
      * con esta manera podemos retornar usuarios en diferentes vistas
+     * 
      * @return
      */
     @ModelAttribute("users")
-    public List<User> setUsers(){
+    public List<User> setUsers() {
         List<User> users = new ArrayList<>();
         users.add(new User("John", "Doe", "john@doe.com"));
         users.add(new User("Jane", "Doe", "jane@doe.com"));
