0c4bc47874051a37e2720e55c288500033518f1b
Victor Jesus Romero Perez <vcoromero@gmail.com>
2023-03-02 13:05:16 -0600

    how to send many variables in route path

--- src/main/java/com/bolsaideas/springboot/app/springbootweb/controllers/VariablePathsController.java
+++ src/main/java/com/bolsaideas/springboot/app/springbootweb/controllers/VariablePathsController.java
@@ -10,10 +10,24 @@ import org.springframework.ui.Model;
 @RequestMapping("/variables")
 public class VariablePathsController {
 
+    @GetMapping({ "", "/" })
+    public String index(Model model) {
+        model.addAttribute("title", "Enviar parametros de la ruta (@PathVariable)");
+        return "variables/index";
+    }
+
     @GetMapping("/string/{text}")
     public String variables(@PathVariable String text, Model model) {
         model.addAttribute("title", "Recibir parametros de la ruta (@PathVariable)");
         model.addAttribute("result", "el texto enviado por la ruta es: " + text);
+        return "variables/see";
+    }
+
+    @GetMapping("/string/{text}/{number}")
+    public String variables(@PathVariable String text, @PathVariable Integer number, Model model) {
+        model.addAttribute("title", "Recibir parametros de la ruta (@PathVariable)");
+        model.addAttribute("result",
+                "el texto enviado por la ruta es: " + text + " y el numero enviado en el path es: " + number);
         return "variables/see";
     }
 }
